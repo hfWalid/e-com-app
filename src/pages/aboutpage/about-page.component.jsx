472e112d5df24c4b9afd39804c4605cf15665f51
Walid HEFHAF <WHEFHAF@pca.ma>
2020-02-23 18:11:07 +0100

    Architecting Components

---
+++ src/pages/aboutpage/about-page.component.jsx
@@ -0,0 +1,13 @@
+// Libraries......
+import React from 'react';
+
+// Components.....
+
+// Stylesheet.....
+import './about-page.styles.scss';
+
+const AboutPage = () => (
+    <div> About Page</div>
+);
+
+export default AboutPage;
